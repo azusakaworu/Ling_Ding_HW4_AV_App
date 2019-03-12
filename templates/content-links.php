e7056fa9b1aee760daea2c38274771bf6a4932fd
LiaVernaet <l_vernaet@fanshaweonline.ca>
2019-03-12 16:07:41 -0400

    worked on homepage and made templates -LV

---
+++ templates/content-links.php
@@ -0,0 +1,45 @@
+<div class="small-12 large-12 columns" id="contentLinks">
+
+<ul class="rowLinks" >
+    <li>
+<a href="#"><img src="images/HOME-MOVIE.jpg" class="imgLinks" alt="settings"></a>
+<a href="#"><img src="images/HOME-TV.jpg" class="imgLinks" alt="profile"></a>
+<a href="#"><img src="images/HOME-MUSIC.jpg" class="imgLinks" alt="menu"></a>
+</li>
+</ul>
+
+<ul class="rowLinks" >
+    <li>
+<p class="imgLinks imageCaptions" alt="settings">Canada's Most Popular Movies</p>
+<p class="imgLinks imageCaptions" alt="settings">All Series TV Shows</p>
+<p class="imgLinks imageCaptions" alt="settings">High Level Original Music OST</p>
+</li>
+</ul>
+
+
+
+
+
+
+
+<div class="small-12 large-12 columns" id="entryLinks">
+
+<ul class="entryrowLinks" >
+    <li>
+<a href="#"><img src="images/child1.jpg" class="entryimgLinks" alt="settings"></a>
+<a href="#"><img src="images/parents-entry.jpg" class="entryimgLinks" alt="profile"></a>
+</li>
+</ul>
+
+<ul class="entryrowLinks" >
+    <li>
+<p class="entryimgLinks entryimageCaptions" alt="settings">Parents Entry</p>
+<p class="entryimgLinks entryimageCaptions" alt="settings">Kids Entry</p>
+</li>
+</ul>
+<!-- 
+</section> -->
+</div>
+
+
+</div>
---
+++ templates/footer.php
@@ -0,0 +1,31 @@
+
+<!-- <div class="small-12 large-6 columns"> -->
+    
+<!-- <section id="footerLinks"> -->
+
+<!-- </div> -->
+<div class="small-12 large-6 columns" id="footerContentLinks">   
+    
+<ul class="footerRowLinks" >
+    
+    <li>
+        
+    <h2 id="footerLogo">ROKU | Flashback</h2>
+<a href="#"><img src="images/social-media-TWITTER.svg" class="imgFooterLinks" alt="settings"></a>
+<a href="#"><img src="images/social-media-FACEBOOK.svg" class="imgFooterLinks" alt="profile"></a>
+<a href="#"><img src="images/social-media-YOUTUBE.svg" class="imgFooterLinks" alt="menu"></a>
+<a href="#"><img src="images/social-media-HI.svg" class="imgFooterLinks" alt="menu"></a>
+</li>
+</ul>
+
+</div>
+<!-- </section> -->
+<div class="small-12 large-12 columns footerBottom">
+    <p>Site Map | Privacy | policy  Terms of use  |  Dispute  | ResolutionTrademark guidelines | LegalAbout Ads & Cookies</p>
+
+    <p>© 2018 Roku, Inc. All rights reserved. ROKU, the ROKU Logo, ROKU TV, ROKU POWERED</p>
+
+</div>
+
+
+
