1a9a6d7d77ad41d0ec2d46936c52f4f201ba6eaa
LiaVernaet <l_vernaet@fanshaweonline.ca>
2019-03-12 16:02:25 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Vue.js Single-File JavaScript Component Demo</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/foundation.css">
+  <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
+
+  <script src="https://unpkg.com/vue"></script>
+  <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+</head>
+<body>
+<?php include('templates/header.php'); ?>
+
+
+<div class="small-12 large-9 large-push-3 large-centered columns">
+        <img src="images/logo.png" id="logo" alt="Logo">
+        </div>
+        <div class="small-12 large-11 large-push-1 large-centered columns">
+        <img src="images/home-img.jpg" id="banner" alt="bannerimg">
+        </div>
+        <div class="small-12 large-4 large-centered columns">
+          <h2 class="bannerText">Everything you need here.</h2>
+          <h2 class="bannerText">Best experience for each family member.</h2>
+
+</div>
+<?php include('templates/content-links.php'); ?>
+        
+
+        <div class="questions">
+
+        <h2>Setting Up Your Roku Account</h2>
+        <hr>
+        <ul>
+        <li><a href="#">What is a Roku account?</a></li>
+        <li><a href="#">How do I create a Roku account?</a></li>
+        <li><a href="#">How do I keep my Roku account secure?</a></li>
+        <li><a href="#">How do I add a Roku streaming device to my Roku account?</a></li>
+</div>
+
+        <div class="questions2">
+
+        <h2>How To: Using Your Roku</h2>
+        <hr>
+        <ul>
+        <li><a href="#">How to add and remove channels</a></li>
+        <li><a href="#">The Roku mobile app</a></li>
+        <li><a href="#">Hotel and dorm connect</a></li>
+        <li><a href="#">How to use private listening</a></li>
+</div>
+        <div class="questions3">
+
+        <h2>Roku® devices: content, or choose to rent or buy. It's up to you.</h2>
+        <hr>
+        <p>Movies, TV shows, news, and music. Blockbusters, big entertainment brands, and niche channels. Watch free or paid subscription content, or choose to rent or buy. It's up to you.</p>
+</div>
+
+
+
+
+     
+        </body>
+
+<?php include('templates/footer.php'); ?>
+
+        </html>
