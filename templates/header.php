ec0ec830c00b0e3b463bb93bf9689d355196f683
LiaVernaet <l_vernaet@fanshaweonline.ca>
2019-03-14 10:24:59 -0400

    Updated files - LV

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,9 @@
                 <div id="headerCon" data-sticky data-options="marginTop:0" style="width:100%">
                     <!--header navigation-->
                   <div class="small-12 large-12 columns" >
+
+      <section class="row">
+      <div class="small-6 large-6 large-pull-2 columns">
                      <!-- <a href="index.html"><img src="images/logo.png" id="logoMobile" alt="Logo"></a> -->
                       <h2 class="hide">Main Navigation</h2>
                         <ul id="mainNav" >
@@ -9,14 +12,21 @@
                                <li><a href="parents.php">Parents</a></li>
                                <li><a href="kids.php">^ Kids</a></li>
                        </ul>
-
-                       <ul id="iconNav" >
-                       <li>
-                             <a href="#"><img src="images/setting.svg" id="settingSVG" alt="settings"></a>
-                             <a href="#"><img src="images/profile.svg" id="profileSVG" alt="profile"></a>
-                             <a href="#"><img src="images/menu.svg" id="menuSVG" alt="menu"></a>
-                             <li>
-                       </ul>
+</div>
+</section>
+<section class="row" id="iconNav">
+<div class="small-4 large-2 large-pull-2 large-push-6 columns">
+                             <a href="#"><img src="images/setting.svg" class="iconNavSVG" alt="settings"></a>
+</div>
+<div class="small-4 large-2 large-push-6 columns">
+                             <a href="#"><img src="images/profile.svg" class="iconNavSVG" alt="profile"></a>
+</div>
+<div class="small-4 large-2 columns">
+                             <a href="#"><img src="images/menu.svg" class="iconNavSVG" alt="menu"></a>
+</div>
+                  
                         </nav>
+</div>
+</section>
                     </div>
 </div>
---
+++ templates/select-movie.php
@@ -0,0 +1,51 @@
+<section id="promoContainer">
+		<h2 class="hide">Promo Area</h2>
+		<section id="promoImage">
+                <h2 id="sectionHeader">Movies for Kids</h2>
+			<h2 class="hide">Summer Promo Image</h2>
+			<img src="images/kid2.png" alt="pic">
+		</section>
+		<section id="promoDesc">
+		<div id="promoContent">
+			<h2 class="hide">Summer Promo Description</h2>
+                        <h2>Zootopia (2016)</h2>
+			
+		<p>From the largest elephant to the smallest shrew, the city of Zootopia is a mammal metropolis where various animals live and thrive. When Judy Hopps (Ginnifer Goodwin) becomes the first rabbit to join the police force, she quickly learns how tough it is to enforce the law. Determined to prove herself, Judy jumps at the opportunity to solve a mysterious case. Unfortunately, that means working with Nick Wilde (Jason Bateman), a wily fox who makes her job even harder.</p>
+			</div>
+		</section>
+	</section>
+
+        <section id="thumbSection">
+ 		<h2 class="hide">Movie Thumbnails</h2>
+   	<div class="small-12 large-12 columns">
+   
+     <!--movie thumbnails-->
+      <section class="row">
+      <h2 class="hide">Secret Life of Pets</h2>
+  	<div class="small-6 large-3 columns">
+  		<img class="thumbPic" src="images/kid1.png">
+                  </div>
+
+                  <h2 class="hide">Zootopia</h2>
+  	<div class="small-6 large-3 columns">
+  		<img class="thumbPic" src="images/kid2.png">
+                  </div>
+
+                  <h2 class="hide">Incredibles 2</h2>
+  	<div class="small-6 large-3 columns">
+  		<img class="thumbPic" src="images/kid3.png">
+                  </div>
+
+                  <h2 class="hide">Inside Out</h2>
+  	<div class="small-6 large-3 columns">
+  		<img class="thumbPic" src="images/kid4.png">
+                  </div>
+
+                  <h2 class="hide">arrow</h2>
+  	<div class="small-6 large-2 columns">
+  		<img class="rightArrow" src="images/mobile-parent-right.svg">
+                  </div>
+
+
+
+                  </section>
